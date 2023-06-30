df743a21342e3236a42eb64f0ccefa2cd2892c84
Marcelo Alves Santos <marcelonecessario@hotmail.com>
2023-06-30 12:31:44 -0300

    Commit: TilePane, FXML Config and Adicionar Controles.

--- leonardoLeitao-exercicios-javafx/src/layout/AppLayout.java
+++ leonardoLeitao-exercicios-javafx/src/layout/AppLayout.java
@@ -41,7 +41,8 @@ public class AppLayout extends Application {
 		//raiz = new TesteBorderPane();
 		//raiz = new TesteFlowPane();
 		//raiz = new TesteGridPane();
-		raiz = new TesteStackPane();
+		//raiz = new TesteStackPane();
+		raiz = new TesteTilePane();
 		//Scene principal = new Scene(new TesteAnchorPane(), 800, 600); // Foi simplificado com o raiz
 		Scene principal = new Scene(raiz, 800, 600);
 		
--- leonardoLeitao-exercicios-javafx/src/module-info.java
+++ leonardoLeitao-exercicios-javafx/src/module-info.java
@@ -1,7 +1,10 @@
 module exerciciosfx {
+	
 	requires javafx.controls;
-	requires javafx.graphics;
+	requires javafx.fxml;
+
 	
 	opens basico;
 	opens layout;
+	opens fxml;
 }
