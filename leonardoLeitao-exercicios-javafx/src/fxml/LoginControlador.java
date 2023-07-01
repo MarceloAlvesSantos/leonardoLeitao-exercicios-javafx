b79bb942603bf8a38e25931a0187e3987a1069c4
Marcelo Alves Santos <marcelonecessario@hotmail.com>
2023-07-01 17:04:52 -0300

    Commit: Fim do javaFX

--- leonardoLeitao-exercicios-javafx/src/fxml/LoginControlador.java
+++ leonardoLeitao-exercicios-javafx/src/fxml/LoginControlador.java
@@ -1,6 +1,9 @@
 package fxml;
 
+import org.controlsfx.control.Notifications;
+
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
@@ -12,8 +15,27 @@ public class LoginControlador {
 	private PasswordField senhaField;
 	
 	public void entrar() {
-		System.out.println(emailField.getText());
-		System.out.println(senhaField.getText());
+		
+		boolean emailValido = emailField.getText().equals("aluno@gmail.com");
+		boolean senhaValida = senhaField.getText().equals("123456");
+		
+		if (emailValido && senhaValida) {
+			System.out.println("Sucesso!!");
+			Notifications.create()
+				.position(Pos.BOTTOM_RIGHT)
+				.title("Login FXML")
+				.text("Login efetuado com sucesso")
+				.showInformation();
+				
+		} else {
+			System.out.println("Falho");
+			Notifications.create()
+				.position(Pos.BOTTOM_RIGHT)
+				.title("Login FXML")
+				.text("Falha no Login")
+				.showError();
+		}
+		
 	}
 
 }
